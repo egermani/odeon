c560fa186ca85838e5c0f7961f6192009d073d1c
brianmock <briannealmock@gmail.com>
2015-10-27 11:50:51 -0500

    Complete reply form functionality

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -28,7 +28,7 @@ class RepliesController < ApplicationController
 
     respond_to do |format|
       if @reply.save
-        format.html { redirect_to @reply, notice: 'Reply was successfully created.' }
+        format.html { redirect_to @reply.critique.review}
         format.json { render :show, status: :created, location: @reply }
       else
         format.html { render :new }
