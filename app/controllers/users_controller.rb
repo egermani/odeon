249e794e104bbc4a2d8d8834d1e73dbec092607b
Erik Germani <erikg885@gmail.com>
2015-10-27 22:55:37 -0500

    Start on authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_filter :authorize, :except => [:new]
 
   # GET /users
   # GET /users.json
@@ -70,5 +71,9 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def authorize
+      
     end
 end
